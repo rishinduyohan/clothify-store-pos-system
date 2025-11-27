0e4536ee92e7f4baad714ada4d9266e42e8d0954
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-27 21:52:08 +0530

    create product DTO and product Entity with relations

--- src/main/java/edu/icet/clothify/model/dto/ProductDTO.java
+++ src/main/java/edu/icet/clothify/model/dto/ProductDTO.java
@@ -1,10 +1,19 @@
 package edu.icet.clothify.model.dto;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class ProductDTO {
-    private String id;
+    private Long productId;
     private String name;
     private String description;
     private String imagePath;
     private Double price;
-
+    private Integer stockQuantity;
+    private String categoryId;
+    private String supplierId;
 }
--- src/main/java/edu/icet/clothify/model/entity/Product.java
+++ src/main/java/edu/icet/clothify/model/entity/Product.java
@@ -1,5 +1,28 @@
 package edu.icet.clothify.model.entity;
 
-public class Product {
+import jakarta.persistence.*;
+import lombok.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String name;
+    private String description;
+    private String imagePath;
+    private Double price;
+    private Integer stockQuantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private String categoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supplier_id", referencedColumnName = "supplier_id")
+    private String supplierId;
 }
