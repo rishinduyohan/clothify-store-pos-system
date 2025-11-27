acda02be5ff6e99c10016cc93c20bd52d7d2458a
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-27 12:23:49 +0530

    create HibernateUtil and hibernate.cfg.xml file

---
+++ src/main/java/edu/icet/ecom/config/HibernateUtil.java
@@ -0,0 +1,18 @@
+package edu.icet.ecom.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final Configuration configuration = new Configuration();
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory(Class getClass){
+        if (null==sessionFactory){
+            configuration.addAnnotatedClass(getClass);
+            configuration.configure();
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+}
