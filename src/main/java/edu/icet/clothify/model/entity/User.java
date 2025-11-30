049a7fae59d5cccc5aaeb2f6d65064172d53d304
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-30 21:54:46 +0530

    create user entity

---
+++ src/main/java/edu/icet/clothify/model/entity/User.java
@@ -0,0 +1,31 @@
+package edu.icet.clothify.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id",nullable = false)
+    private Employee employee;
+}
