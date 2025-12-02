00606dc7d0147ab1da1e1b5674a44890f4ac0154
Rishindu Yohan <rishinduyohan@gmail.com>
2025-12-02 12:01:35 +0530

    create order and Order detail entities with db connection

---
+++ src/main/java/edu/icet/clothify/model/entity/Order.java
@@ -0,0 +1,29 @@
+package edu.icet.clothify.model.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "order_date")
+    private LocalDateTime date;
+
+    @Column(name = "total_amount")
+    private String totalAmount;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+}
