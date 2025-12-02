c85c34b9b67a61f8299ce552db933e626d78ea99
Rishindu Yohan <rishinduyohan@gmail.com>
2025-12-02 12:04:38 +0530

    update product_cart.fxml UI

--- src/main/java/edu/icet/clothify/controller/ProductCardController.java
+++
@@ -1,9 +0,0 @@
-package edu.icet.clothify.controller;
-
-import javafx.event.ActionEvent;
-
-public class ProductCardController {
-
-    public void btnAddToCartOnAction(ActionEvent actionEvent) {
-    }
-}
