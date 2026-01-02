32f9cc1e5aef9fbd16849f1da83b88b4f2c00ba1
Rishindu Yohan <rishinduyohan@gmail.com>
2026-01-02 13:20:24 +0530

    create report-genarate controller and add jasper report dependancy

---
+++ src/main/java/edu/icet/clothify/controller/ReportController.java
@@ -0,0 +1,54 @@
+package edu.icet.clothify.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class ReportController {
+
+    @FXML
+    private Button btnDailySales;
+
+    @FXML
+    private Button btnMonthlyReport;
+
+    @FXML
+    private Button btnSupplierReport;
+
+    @FXML
+    private Button btnTopSelling;
+
+    @FXML
+    private VBox cardDailySales;
+
+    @FXML
+    private VBox cardMonthlyRevenue;
+
+    @FXML
+    private VBox cardSupplierList;
+
+    @FXML
+    private VBox cardTopSelling;
+
+    @FXML
+    void btnDailySalesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMonthlyReportOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierReportOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTopSellingOnAction(ActionEvent event) {
+
+    }
+
+}
