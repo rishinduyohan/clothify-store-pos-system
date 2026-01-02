e85cb2d109a6e1e65b5001f15a9bf0af731570c5
Rishindu Yohan <rishinduyohan@gmail.com>
2026-01-02 16:58:04 +0530

    create date picker controller

---
+++ src/main/java/edu/icet/clothify/controller/DatePickerController.java
@@ -0,0 +1,29 @@
+package edu.icet.clothify.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+
+public class DatePickerController {
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnGenerate;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    void btnCancelOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGenerateOnAction(ActionEvent event) {
+
+    }
+
+}
