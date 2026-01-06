3ef8269451af5d8f8a993ead891a4948df3a9323
Rishindu Yohan <rishinduyohan@gmail.com>
2026-01-06 17:34:27 +0530

    create user service and repository classes

---
+++ src/main/java/edu/icet/clothify/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,35 @@
+package edu.icet.clothify.repository.impl;
+
+import edu.icet.clothify.config.HibernateUtil;
+import edu.icet.clothify.model.entity.User;
+import edu.icet.clothify.repository.UserRepository;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+public class UserRepositoryImpl implements UserRepository {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    User user = new User();
+
+    @Override
+    public boolean addUser(User user) {
+        Transaction transaction = session.beginTransaction();
+        session.persist(user);
+        transaction.commit();
+        return true;
+    }
+
+    @Override
+    public User getUser(String email) {
+        try {
+            String hql = "FROM User WHERE email = :userEmail";
+            Query<User> query = session.createQuery(hql, User.class);
+            query.setParameter("userEmail", email);
+            return query.uniqueResult();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
