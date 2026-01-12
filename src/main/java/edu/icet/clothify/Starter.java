b87e2d8352df7ee51aa09f4ac47ce4a3385ab182
Rishindu Yohan <rishinduyohan@gmail.com>
2026-01-12 14:35:57 +0530

    Add icons to login and dashboard stages

--- src/main/java/edu/icet/clothify/Starter.java
+++ src/main/java/edu/icet/clothify/Starter.java
@@ -3,6 +3,7 @@ package edu.icet.clothify;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Starter extends Application {
@@ -14,6 +15,7 @@ public class Starter extends Application {
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login_page.fxml"))));
         stage.setTitle("Login Form");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/login.png")));
         stage.show();
     }
 }
